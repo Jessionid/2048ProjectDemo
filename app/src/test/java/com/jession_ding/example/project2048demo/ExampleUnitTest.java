e446469254c2bc5f46e7b5831c89fb5f15bb46a7
Jessionid <ds972869347@163.com>
2017-12-12 14:49:15 +0800

    Initial commit

---
+++ app/src/test/java/com/jession_ding/example/project2048demo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.jession_ding.example.project2048demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
